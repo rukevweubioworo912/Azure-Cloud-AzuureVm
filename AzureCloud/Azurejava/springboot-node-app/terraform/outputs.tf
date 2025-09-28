ab13deee460340a2814e6a95405ce401a57676f3
Rukevwe Ubioworo <ubiorukevwe@outlook.com>
2025-09-28 08:07:44 +0000

    create teh gitaction workflow

--- AzureCloud/Azurejava/springboot-node-app/terraform/outputs.tf
+++ AzureCloud/Azurejava/springboot-node-app/terraform/outputs.tf
@@ -1,3 +1,4 @@
-output "app_url" {
-  value = azurerm_app_service.app.default_site_hostname
+output "app_service_url" {
+  description = "The default URL of the deployed Azure App Service"
+  value       = "https://${azurerm_app_service.app.default_site_hostname}"
 }
--- AzureCloud/Azurejava/springboot-node-app/terraform/variables.tf
+++ AzureCloud/Azurejava/springboot-node-app/terraform/variables.tf
@@ -1,15 +1,39 @@
 variable "resource_group_name" {
-  default = "myResourceGroup"
+  type = string
 }
 
 variable "location" {
+  type    = string
   default = "eastus"
 }
 
-variable "app_name" {
-  default = "springboot-node-app"
+variable "acr_name" {
+  type = string
 }
 
-variable "acr_name" {
-  default = "springbootnodeacr"
+variable "acr_sku" {
+  type    = string
+  default = "Basic"
+}
+
+variable "app_service_plan_name" {
+  type = string
+}
+
+variable "app_service_plan_sku" {
+  type    = string
+  default = "B1"
+}
+
+variable "app_service_name" {
+  type = string
+}
+
+variable "docker_image_name" {
+  type = string
+}
+
+variable "docker_image_tag" {
+  type    = string
+  default = "latest"
 }
